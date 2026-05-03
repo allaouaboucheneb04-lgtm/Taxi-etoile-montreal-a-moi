514cba02a10a893a433448af5665b736895c5a6c
Allaoua Boucheneb <allaouaboucheneb04@gmail.com>
2026-05-03 01:01:33 -0400

    new

---
+++ app.js
@@ -0,0 +1,7 @@
+const SERVICE_ID = 'service_5phpu0d';
+const TEMPLATE_ID = 'template_06gymkw';
+const PUBLIC_KEY = 'W-3rUaqJdvEjPE1J0';
+
+if (window.emailjs) {
+  window.emailjs.init(PUBLIC_KEY);
+}
